6c3a5c5457cc5572f2909fa8e482e319ff04db20
권민지 <kwonminji@gwonminjiui-MacBookPro.local>
2020-07-01 17:46:16 +0900

    #1.1 Creating GraphQL Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("hello");
+require("dotenv").config();
+import {GraphQLServer} from "graphql-yoga";
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hi"
+    }
+};
+
+const server = new GraphQLServer({typeDefs, resolvers});
+
+// 옵션 추가 - port, callback 함수
+server.start({port:PORT}, () => console.log(`Server running on port http://localhost:${PORT}`));
