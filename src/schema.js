c19bf59a236e06ca41392e03c0937710fc9d7143
권민지 <kwonminji@gwonminjiui-MacBookPro.local>
2020-07-02 19:25:37 +0900

    #2.4 Resolvers with Prisma

--- src/schema.js
+++ src/schema.js
@@ -2,6 +2,9 @@ import {makeExecutableSchema} from "graphql-tools";
 import {fileLoader, mergeResolvers, mergeTypes} from "merge-graphql-schemas";
 import path from "path"
 
+// fileLoader 함수의 결과물, file Loader(파일 경로)
+// ** : 모든 폴더, *.graphql : 모든 .graphql 파일
+// api 폴더 밑의 모든 폴더에서 graphql 파일만 가져옴
 const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
 const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
 
