4a2a80f1ca50b40573a7aed4c5665f84e71f11c0
jjadekwon <jjade.kwon@gmail.com>
2020-07-27 23:17:59 +0900

    #3.11 follow unfollow Resolver

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,6 +7,12 @@ export default {
             const {email, secret} = args;
             const user = await prisma.user({email});
             if (user.loginSecret === secret) {
+                // 로그인이 확인되면 loginSecret 삭제
+                await prisma.updateUser({
+                    where: { id: user.id },
+                    data: { loginSecret: "" }
+                });
+
                 return generateToken(user.id);
             } else {
                 throw Error("Wrong email/secret combination.");
