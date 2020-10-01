ec42e21e70f93a2a0273ac2009d7b88c8f092955
jjadekwon <jjade.kwon@gmail.com>
2020-10-01 19:40:56 +0900

    add middleware

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,11 +1,18 @@
-import {prisma} from "../../../../generated/prisma-client";
+import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-    Mutation: {
-        createAccount: async(_, args) => {
-            const { username, email, firstName = "", lastName = "", bio = "" } = args;
-            const user = await prisma.createUser({username, email, firstName, lastName, bio});
-            return user;
-        }
-    }
-}
+  Mutation: {
+    createAccount: async (_, args) => {
+      const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({
+        OR: [{ username }, { email }],
+      });
+      if (exists) {
+        throw Error("This username / email is already taken");
+      }
+
+      await prisma.createUser({ username, email, firstName, lastName, bio });
+      return true;
+    },
+  },
+};
--- src/server.js
+++ src/server.js
@@ -6,17 +6,21 @@ import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
+import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ 
-    schema, 
-    context: ({request}) => ({request, isAuthenticated })
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({ request, isAuthenticated }),
 });
 
 // express 서버에서 logger 미드웨어 (morgan)을 사용
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 // 옵션 추가 - port, callback 함수
-server.start({ port:PORT }, () => console.log(`✅Server running on port http://localhost:${PORT}`));
+server.start({ port: PORT }, () =>
+  console.log(`✅Server running on port http://localhost:${PORT}`)
+);
